b759cc329706baf9ea1765fa154a49181b14886c
freshteapot <chris@freshteapot.net>
2021-10-26 01:44:42 +0200

    Support to include hashes
    - If you make a mistake with the subject, you can specify the hash now

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"os"
 	"regexp"
 	"strings"
 	"time"
@@ -20,16 +20,27 @@ type ChangeLog struct {
 }
 
 func main() {
-	path := os.Args[1]
+	hashesPtr := flag.String("hashes", "", `List of hashes to include, split by ",""`)
+
+	flag.Parse()
+
+	path := flag.Args()[0]
 	r, _ := git.PlainOpen(path)
 	ref, _ := r.Head()
 	cIter, _ := r.Log(&git.LogOptions{From: ref.Hash()})
 	re := regexp.MustCompile(`(?s)\((.*)\)`)
 
+	includeHashes := strings.Split(*hashesPtr, ",")
+
 	chanelog := make([]ChangeLog, 0)
 	_ = cIter.ForEach(func(c *object.Commit) error {
-		if !strings.HasPrefix(c.Message, "Change") {
-			return nil
+
+		skip := hasHash(c.Hash.String()[:7], includeHashes)
+		if !skip {
+			// Check for prefix "Change"
+			if !strings.HasPrefix(c.Message, "Change") {
+				return nil
+			}
 		}
 
 		parts := strings.Split(c.Message, "\n")
@@ -51,7 +62,7 @@ func main() {
 			What: message,
 			When: c.Committer.When.Format(time.RFC3339),
 			PR:   pr,
-			Hash: c.Hash.String()[:6],
+			Hash: c.Hash.String()[:7],
 		})
 
 		return nil
@@ -59,4 +70,13 @@ func main() {
 
 	b, _ := json.Marshal(chanelog)
 	fmt.Println(string(b))
+}
+
+func hasHash(hash string, hashes []string) bool {
+	for _, search := range hashes {
+		if search == hash {
+			return true
+		}
+	}
+	return false
 }
